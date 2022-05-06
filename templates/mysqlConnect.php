787080b804a9898fc565a8abd388ef66826cebfd
Yuliana07 <71000001+Yuliana07@users.noreply.github.com>
2022-05-06 15:31:01 +0900

    Add files via upload

---
+++ templates/mysqlConnect.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$BDname = "bd_blog";
+
+$mysqli = new mysqli($servername, $username, $password, $BDname);
+
+if ($mysqli -> connect_error) {
+    printf("Соединение не удалось: %s\n", $mysqli -> connect_error);
+    exit();
+};
